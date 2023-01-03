24a2adac849ed2bcf3420caaade7616b4db31641
AlexanderWinston <113539276+AlexanderWinston@users.noreply.github.com>
2023-01-02 22:45:29 -0600

    still working on js function for lure ball

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const countElement = document.getElementById("count");
+
 let count = 0;
+
 countElement.innerText = "Pokéball Count:" + count;
+
 function increaseCount() {
   count++;
   countElement.innerText = "Pokéball Count:" + count;
 }
 
 function increaseCountLure() {
-  if(countElement.innerText >= 50) {
-    countElement.innerText - 50;
-    count +2;
+  if (count >= 50){
+  count++;
+  countElement.innerText = "Pokéball Count:" + count;
   }
 }
